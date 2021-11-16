7dd172bbb8303f23f72d649876d4dfdfd86629e2
Kydryashova-Nastya <avk2001avk@gmail.com>
2021-11-16 15:45:17 +0300

    login

--- travelers_dream/urls.py
+++ travelers_dream/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="employees"),
+    path('', views.Login.as_view(), name="login"),
+    path('employees', views.index, name="employees"),
     path('clients', views.clients, name="clients"),
     path('create-employee', views.create_employee, name="createEmployee"),
     path('employee/<int:id>', views.employee, name="employee"),
     path('create-client', views.create_client, name="createClient"),
     path('client/<int:id>', views.client, name="client"),
     # path('accounts/', include('django.contrib.auth.urls')),
-    path('login', views.Login.as_view(), name="login")
 ]
